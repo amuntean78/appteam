36ce1a7a620adb3627d1107c7baa8f2c61a22bb3
sabinchirila <p.lwcyan_94@yahoo.com>
2016-09-20 11:01:55 +0300

    Revert: Test App

---
+++ src/main/java/com/app/AppTeamApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AppTeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppTeamApplication.class, args);
+    }
+}
